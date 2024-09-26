db57686ea498a1c7b340f3b9f60e35353f7c1396
amisharif <sharif520@gmail.com>
2024-09-26 14:28:18 +0600

    Add project files.

---
+++ PresentationLayer/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+
+        public IActionResult Index(string categoryName)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(Guid ID)
+        {
+
+            return View();
+
+        }
+    }
+}
